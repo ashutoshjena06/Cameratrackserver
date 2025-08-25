1c340621e04a5a52638f10b2fcac7f8b32e1bbe5
Ashutosh jena <ashutoshj261@gmail.com>
2025-08-25 18:41:18 +0530

    vercel url added in cors

--- index.js
+++ index.js
@@ -21,7 +21,15 @@ connectDB().then(() => {
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://cameratrackclient.vercel.app/"],
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
 
